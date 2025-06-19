a7bf3630be12346684ac23ffd7c19ada7fc5ca3b
Rutam Bhagat <rutambhagat@gmail.com>
2025-06-19 11:36:00 -0700

    feat: add mpc math server to main

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import asyncio
 
 from dotenv import load_dotenv
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain_openai import ChatOpenAI
+from langgraph.prebuilt import create_react_agent
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
 
 load_dotenv()
+
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args=[
+        "/home/voldemort/Desktop/Code/python_projects/mcp-servers/langchain-mcp-adapters/servers/math-server.py"
+    ],
+)
 
 
 async def main():
